152e613297ce083109c74e01f49c4ca14bc6c683
Diarmuid O Connor <doconnor@wit.ie>
2022-01-01 17:23:31 +0000

    Add to favourites logic completed; Dummy favourites page.

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -12,6 +12,7 @@ import CalendarIcon from "@material-ui/icons/CalendarTodayTwoTone";
 import StarRateIcon from "@material-ui/icons/StarRate";
 import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
@@ -25,10 +26,30 @@ const useStyles = makeStyles({
 export default function MovieCard(props) {
   const classes = useStyles();
   const movie = props.movie;
+
+  const handleAddToFavourite = (e) => {
+    e.preventDefault();
+    props.selectFavourite(movie.id);
+  };
+
   return (
     <Card className={classes.card}>
-      <CardHeader className={classes.header} title={movie.title} />
-      <CardMedia
+     <CardHeader
+      className={classes.header}
+      avatar={
+        movie.favourite ? (
+          <Avatar className={classes.avatar}>
+            <FavoriteIcon />
+          </Avatar>
+        ) : null
+      }
+      title={
+        <Typography variant="h5" component="p">
+          {movie.title}{" "}
+        </Typography>
+      }
+    />
+       <CardMedia
         className={classes.media}
         image={
           movie.poster_path
@@ -53,9 +74,9 @@ export default function MovieCard(props) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={null}>
-          <FavoriteIcon color="primary" fontSize="large" />
-        </IconButton>
+      <IconButton aria-label="add to favourites" onClick={handleAddToFavourite}>
+        <FavoriteIcon color="primary" fontSize="large" />
+    </IconButton>
         <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
